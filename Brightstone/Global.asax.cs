b76d16770fac31cb27e4cdb9c6299ac2f53f8608
kruglenya <Kruglenya.A.Va@sberbank.ru>
2018-09-10 13:28:32 +0300

    push

---
+++ Brightstone/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Brightstone
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+      //  private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
